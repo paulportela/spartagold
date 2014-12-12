1444671c6fbfefcfbf0649d55397cc423d1e49dc
Paul Portela <wcoportelap@gmail.com>
2014-12-12 15:37:49 -0800

    fixing getinput.java

--- spartagold/wallet/frontend/GetInput.java
+++ spartagold/wallet/frontend/GetInput.java
@@ -1,10 +1,5 @@
-package spartagold.wallet.frontend;
 
-
-<<<<<<< HEAD
-=======
 package spartagold.wallet.frontend;
->>>>>>> 5cdab83919b01deea187ad3914bb6d1ba1204760
 import java.util.Scanner;
 /**
  *
